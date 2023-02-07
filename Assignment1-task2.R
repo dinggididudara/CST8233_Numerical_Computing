0072a9ecc7b81eb5ebcb752a86eaac2b4007ad17
Soomin Lee <dinggididududu@gmail.com>
2023-02-07 01:17:17 -0500

    assignment 1 task 2 init

--- Assignment1-task2.R
+++ Assignment1-task2.R
@@ -1,14 +1,34 @@
 # Soomin Lee 040899389 CST8233 304
 # Task 2 : Vectors, Functions, Series ---------------------------
+
+# <- function (x){
+#   return (cos(0.5)*x * exp(0.1*x))
+#  }
+
 library("PolynomF")
 
-MyIntCal <- function (x){
-  return (cos(0.5)*x * exp(0.1*x))
-}
+# data points
+x <- c(pi, 6.678, 3*pi, 12.961, 5*pi, 19.244, 7*pi)
+y <- c(0,-1.921, 0, 3.584, 0, -6.718, 0)
+
+MyIntCal <- approxfun(x,y)    # create interpolating function
+
+pdf(file="Lab\\MyIntFig.pdf", width=4, height=2) # create 4 x 2 figure and
+plot(x, y)      
+curve(MyIntCal,add=TRUE)
+points(x,y, col="red", pch = 16)  # lot each of these polynomials
+dev.off()         # close to create the file
+ 
+pf_x <- poly_calc(x,y)    # find interpolating function
+plot(pf_x)
+curve(MyIntCal, add=TRUE, col="green")
 
 
-# what is degree of interpolating function
-MyIntFig.pdf <- plot()# create 4 x 2 figure and ;lot each of these ploynomials
-pf_x <- ploy.calc(MyIntCal())# find interpolating function
 MyIntCal(15)
-MyIntCal(24)
+MyIntCal(24)    
+
+
+
+
+
+ 
