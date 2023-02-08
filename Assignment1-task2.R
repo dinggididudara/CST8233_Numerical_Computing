5dd24412cd9ee64a2f627c23d023f74133be64ee
Soomin Lee <dinggididududu@gmail.com>
2023-02-08 13:42:51 -0500

    Assignment1 moidified

--- Assignment1-task2.R
+++ Assignment1-task2.R
@@ -11,24 +11,39 @@ library("PolynomF")
 x <- c(pi, 6.678, 3*pi, 12.961, 5*pi, 19.244, 7*pi)
 y <- c(0,-1.921, 0, 3.584, 0, -6.718, 0)
 
-MyIntCal <- approxfun(x,y)    # create interpolating function
+# create interpolating function
+MyIntCal <- approxfun(x,y)   
 
-lag <- lagrange(x,y)          # find derivation
+# find derivation
+lag <- lagrange(x,y)         
 deriv(lag)
 
-pdf(file="Lab\\MyIntFig.pdf", width=4, height=2) # create 4 x 2 figure and
-plot(x, y)      
-curve(MyIntCal,add=TRUE)
-points(x,y, col="red", pch = 16)  # lot each of these polynomials
+
+# create figure
+pdf(file="Lab\\MyIntFig.pdf", width=10, height=10 )
+
+plot_lag <- function(a, b, i) {
+  lag <- lagrange(a, b)
+  plot(lag, xlim = c(min(x), max(x)), ylim = c(min(y), max(y)), 
+       main = paste("Lagrange Polynomial", i))
+}
+par(mfrow=c(4,2)) # add area
+
+for(i in 1:6){    # make multiple plots
+  plot_lag(x[i:(i+1)], y[i:(i+1)], i)
+}
+
 dev.off()         # close to create the file
  
-pf_x <- poly_calc(x,y)    # find interpolating function
-plot(pf_x)
-curve(MyIntCal, add=TRUE, col="green")
 
+# find interpolating function
+pf_x <- poly_calc(x,y)    
 
+# find the result
 MyIntCal(15)
-MyIntCal(24)    
+pf_x(15)
+MyIntCal(24)
+pf_x(24)
 
 
 
